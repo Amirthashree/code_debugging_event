2e79e191c089b4d546cb7484d880213f6dfccc94
Amirthashree <kavithaacsl@gmail.com>
2026-07-31 14:35:24 +0530

    Add contest management routes to contestRoutes.js

--- server/routes/contestRoutes.js
+++ server/routes/contestRoutes.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { getContestStatus, updateContestState } = require('../controllers/contestController');
+const {
+  getContestStatus,
+  updateContestState,
+  startContest,
+  pauseContest,
+  resumeContest,
+  endContest
+} = require('../controllers/contestController');
 const { protect } = require('../middleware/authMiddleware');
 const { adminOnly } = require('../middleware/adminMiddleware');
 
 router.get('/status', protect, getContestStatus);
 router.put('/update', protect, adminOnly, updateContestState);
+
+router.put('/start', protect, adminOnly, startContest);
+router.put('/pause', protect, adminOnly, pauseContest);
+router.put('/resume', protect, adminOnly, resumeContest);
+router.put('/end', protect, adminOnly, endContest);
 
 module.exports = router;
